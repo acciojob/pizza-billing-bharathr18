9c4677da819c01e1583c2b738e9dd00a4d85d8e0
Bharath R <bharathr6362@gmail.com>
2022-12-15 20:35:38 +0530

    Pizza Billing Tick

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,40 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int cheesePrice;
+    private int toppingsPrice;
+    private int takeAwayPrice;
+
+    boolean isCheeseAdded;
+    boolean isToppingsAdded;
+    boolean isTakeAwayAdded;
+    boolean isBillGenerated;
+
+  /*  public Node(int data)
+    {
+        this.data = data;
+    }
+
+   */
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+
+        this.cheesePrice = 80;
+        this.takeAwayPrice = 20;
+
+        if(isVeg == true)
+        {
+            this.price = 300;
+            this.toppingsPrice = 70;
+        }
+        else {
+            this.price = 400;
+            this.toppingsPrice = 120;
+        }
+
+        this.isTakeAwayAdded = false;
     }
 
     public int getPrice(){
@@ -29,6 +59,26 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
+
+        if(isBillGenerated == false)
+        {
+            if(isCheeseAdded == true)
+            {
+                this.bill = "Extra Cheese Added: " + this.cheesePrice + "\n";
+            }
+            if(isToppingsAdded == true)
+            {
+                this.bill = "Extra Toppings Added: " + this.toppingsPrice + "\n";
+            }
+            if(isTakeAwayAdded == true)
+            {
+                this.bill = this.bill + "Paperbag Added: " + this.takeAwayPrice + "\n";
+            }
+
+            this.bill = this.bill + "Total Price: " + this.price + "\n";
+
+            isBillGenerated = true;
+        }
         return this.bill;
     }
 }
